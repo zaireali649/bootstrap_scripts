d8a68e28642a9d1b413ae322e234abf1d72ac3f8
Zaire Ali <zaireali649@gmail.com>
2022-05-25 19:56:20 -0400

    Update apache2.sh

--- apache2.sh
+++ apache2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # package updates
-yum check-update
-yum update 
+#yum check-update
+#yum update 
 # apache installation, enabling and status check
 yum -y install httpd
 systemctl start httpd
@@ -11,10 +11,11 @@ cd /var/www/html/
 echo '<!DOCTYPE html>' > index.html
 echo '<html>' >> index.html
 echo '<head>' >> index.html
-echo '<title>Apache Web Server launched on CLI</title>' >> index.html
+echo '<title>Apache Web Server launched on CI/CD Pipeline</title>' >> index.html
 echo '<meta charset="UTF-8">' >> index.html
 echo '</head>' >> index.html
 echo '<body>' >> index.html
-echo '<h1>Week 6 Project</h1>' >> index.html
+echo '<h1>Apache Web Server launched on CI/CD Pipeline</h1>' >> index.html
+echo '<p>Github -> CodeBuild -> EC2</p>' >> index.html
 echo '</body>' >> index.html
 echo '</html>' >> index.html
